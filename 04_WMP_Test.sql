3cf47c2c1dd6789fb4376ad62343d75353abd7ed
Ahmad Hoghooghi <ahmad.hoghooghi@gmail.com>
2019-05-18 14:58:25 +0430

    Update wmp-test.vpp and sql file

---
+++ 04_WMP_Test.sql
@@ -0,0 +1,8 @@
+drop table WMPTEST_CHANGE_LOG;
+drop table WMPTEST_CHANGE_LOG_LOCK;
+
+drop table TST_MSG;
+drop table TST_MSG_TYPE;
+drop table TST_STRINGIDENTIFIER;
+drop table TST_STUDENT;
+drop table TST_SCHOOL;
