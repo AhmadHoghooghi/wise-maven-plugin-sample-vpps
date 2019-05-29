8f20a0347d4696caf085ee016b92d4e615f7c0a7
Ahmad Hoghooghi <ahmad.hoghooghi@gmail.com>
2019-05-29 12:20:33 +0430

    add many to many, update adaptive

--- 04_WMP_Test.sql
+++ 04_WMP_Test.sql
@@ -1,8 +1,22 @@
 drop table WMPTEST_CHANGE_LOG;
 drop table WMPTEST_CHANGE_LOG_LOCK;
-
+--
 drop table TST_MSG;
 drop table TST_MSG_TYPE;
-drop table TST_STRINGIDENTIFIER;
+-- one to many
 drop table TST_STUDENT;
 drop table TST_SCHOOL;
+
+-- one to one
+drop table TST_CAR cascade constraints ;
+drop table TST_ENGINE cascade constraints ;
+
+-- many to many
+drop table TST_POST_TAG;
+drop table TST_BlogPost;
+drop table TST_TAG;
+
+--
+drop table TST_STRINGIDENTIFIER;
+drop table TST_Validations;
+drop table TST_CriticInfo;
